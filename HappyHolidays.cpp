5fa39841d3a360457b51307bd9859839da2357d2
Martin Robinson <0x4d52@gmail.com>
2020-12-29 18:20:52 +0000

    Add the ability to pass a vector of message atoms

--- HappyHolidays.cpp
+++ HappyHolidays.cpp
@@ -1,9 +1,14 @@
 #include <string>
 #include <iostream>
+#include <vector>
+
+namespace OOO
+{
 
 using C = decltype (std::cout)&;
 using M = std::string;
 using B = bool;
+
 
 static int r1 = 0;
 static int r2 = 0;
@@ -11,55 +16,25 @@ B b2 = false;
 B b3 = false;
 C _ = std::cout;
 
-static void ___ (int n)
-{
-    if (n > 0)
-        _ << M (n, '\n');
-}
+struct MM { MM (M mm, int nn = 1, int ww = 0) : m (mm), n (nn), w (ww) {} M m; int n; int w; };
+using MMM = std::vector<MM>;
 
-static void __ (M m, int n = 1, int w = 0)
-{
-    _.width (w <= 0 ? 0 : w + m.length());
-    _ << m;
-    _.width (0);
-    ___ (n);
-}
+static void _0 (int n) { if (n > 0) _ << M (n, '\n'); }
+static void _8 (M m, int n = 1, int w = 0) { _.width (w <= 0 ? 0 : w + m.length()); _ << m; _.width (0); _0 (n); }
+static void _8_ (MMM mmm) { for (const auto& mm : mmm) _8 (mm.m, mm.n, mm.w); }
 
 struct O
 {
-    O()
-    : b1 (true)
-    {
-        ++r1;
-    }
-    
-    ~O()
-    {
-        if (b1)
-        {
-            if (--r1 == 0)
-            {
-                b2 = false;
-                b3 = false;
-            }
-            
-            r2 = 0;
-        }
-    }
+    O() : b1 (true) { ++r1; }
+    ~O() { if (! b1) return; if (--r1 == 0) b2 = b3 = false; r2 = 0; }
     
     O operator() (const O& oo) const
     {
         if (r1 == 3 && b1 && ! b2)
         {
-            ___ (2);
-            __ ("{", 1, 14);
-            __ ("O _,o ;", 2, 11);
-            __ ("_.o", 1, 12);
-            __ (">>", 1, 14);
-            __ ("*", 1, 14);
-            __ ("(", 1, 14);
-            b2 = true;
-            b3 = true;
+            _0 (2);
+            _8_ ({{ "{", 1, 14 }, { "O _,o ;", 2, 11 }, { "_.o", 1, 12 }, { ">>", 1, 14 }, { "*", 1, 14 }, { "(", 1, 14 }});
+            b2 = b3 = true;
         }
         
         ++r2;
@@ -72,19 +47,9 @@ struct O
         return ooo;
     }
     
-    O operator+ (const O& oo) const
-    {
-        O ooo;
-        ooo.t = t + "+" + oo.t;
-        return ooo;
-    }
-    
-    O operator- (const O& oo) const
-    {
-        O ooo;
-        ooo.t = t + "-" + oo.t;
-        return ooo;
-    }
+    O operator+ (const O& oo) const { O ooo; ooo.t = t + "+" + oo.t; return ooo; }
+    O operator- (const O& oo) const { O ooo; ooo.t = t + "-" + oo.t; return ooo; }
+    void operator>> (M t) const { _ << t; }
 
     M operator*() const
     {
@@ -92,31 +57,17 @@ struct O
 
         if (b4)
         {
-            ___ (2);
-            __ ("{", 1, 14);
-            __ ("O o ;", 2, 11);
-            __ ("_.o", 1, 14);
-            __ ("<<", 1, 13);
-            __ ("*", 1, 14);
-            __ ("(", 1, 14);
+            _0 (2);
+            _8_ ({{ "{", 1, 14 }, { "O o ;", 2, 11 }, { "_.o", 1, 14 }, { "<<", 1, 13 }, { "*", 1, 14 }, { "(", 1, 14 }});
             b2 = true;
         }
 
-        __ ("(" + t + ")", 1, 12);
+        _8 ("(" + t + ")", 1, 12);
         
         if (b4 || b3)
-        {
-            __ (")", 1, 14);
-            __ (";", 1, 14);
-            __ ("}", 1, 14);
-        }
+            _8_ ({{ ")", 1, 14 }, { ";", 1, 14 }, { "}", 1, 14 }});
         
         return M (9, ' ') + m + M (2, '\n');
-    }
-    
-    void operator>> (M t) const
-    {
-        _ << t;
     }
     
     O& o = *this;
@@ -163,9 +114,11 @@ static void east()
               )
               ;
               }
+}
 
 int main (int argc, char* argv[])
 {
+    using namespace OOO;
     west();
     east();
     return 0;
